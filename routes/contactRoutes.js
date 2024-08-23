376346f975976c64c917ee93b39540104604b49c
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-08-23 16:23:53 +0530

    Add Project

---
+++ routes/contactRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
+
+router.use(validateToken);
+
+router
+    .get("/", getContacts)
+    .post("/", createContact);
+
+router
+    .get("/:id", getContact)
+    .put("/:id", updateContact)
+    .delete("/:id", deleteContact);
+
+module.exports = router;
